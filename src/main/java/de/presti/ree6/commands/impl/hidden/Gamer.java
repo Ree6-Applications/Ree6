0d5cc11e474d9b9f9260128b0b505780d3f523a9
DxsSucuk <dxssucuk@hotmail.com>
2021-08-22 21:11:49 +0200

    Prerelease 1.4.9
    = Fixed Gamer Command

--- src/main/java/de/presti/ree6/commands/impl/hidden/Gamer.java
+++ src/main/java/de/presti/ree6/commands/impl/hidden/Gamer.java
@@ -17,7 +17,7 @@ public class Gamer extends Command {
     @Override
     public void onPerform(Member sender, Message messageSelf, String[] args, TextChannel m, InteractionHook hook) {
         sendMessage(sender.getId().equalsIgnoreCase("465586551955652608") ? "Hello Xasus im really happy to see you here!" : sender.getId().equalsIgnoreCase("321580743488831490") ? "Lovely Presti nice to see you here im really happy that you are here!" : sender.getId().equalsIgnoreCase("397462379992055819") ? "David! You're like family i love!" : "Who tf are you?", m, hook);
-        m.getHistoryBefore(messageSelf.getId(), 1).complete().getRetrievedHistory().get(0).reply("https://images.ree6.de/xasus.png").queue();
+        m.getHistoryBefore(messageSelf.getId(), 1).complete().getRetrievedHistory().get(0).reply("https://images.ree6.de/xasus").queue();
         deleteMessage(messageSelf);
     }
 }
