048c36dc5bbf1408ad62cadbb6f44b599bcc4650
Presti <dxssucuk@hotmail.com>
2022-06-27 17:00:46 +0200

    Release

--- src/main/java/de/presti/ree6/main/Main.java
+++ src/main/java/de/presti/ree6/main/Main.java
@@ -98,7 +98,7 @@ public class Main {
         // Create a RayGun Client to send Exception to an external Service for Bug fixing.
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             RaygunClient raygunClient = new RaygunClient(instance.config.getConfiguration().getString("raygun.apitoken"));
-            raygunClient.setVersion("1.7.12");
+            raygunClient.setVersion("1.7.13");
         });
 
         // Create a new connection between the Application and the SQL-Server.
@@ -132,7 +132,7 @@ public class Main {
 
         // Create a new Instance of the Bot, as well as add the Events.
         try {
-            BotWorker.createBot(BotVersion.PUBLIC, "1.7.12");
+            BotWorker.createBot(BotVersion.PUBLIC, "1.7.13");
             instance.musicWorker = new MusicWorker();
             instance.addEvents();
         } catch (Exception ex) {
