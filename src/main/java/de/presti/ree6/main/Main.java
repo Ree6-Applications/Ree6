d0088b7cfd5372c31ddb0944e4f96b90bb975be9
DxsSucuk <dxssucuk@hotmail.com>
2022-07-11 23:26:32 +0200

    * Version bump.

--- src/main/java/de/presti/ree6/main/Main.java
+++ src/main/java/de/presti/ree6/main/Main.java
@@ -99,7 +99,7 @@ public class Main {
         // Create a RayGun Client to send Exception to an external Service for Bug fixing.
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             RaygunClient raygunClient = new RaygunClient(instance.config.getConfiguration().getString("raygun.apitoken"));
-            raygunClient.setVersion("1.7.16");
+            raygunClient.setVersion("1.7.17");
         });
 
         // Create a new connection between the Application and the SQL-Server.
@@ -133,7 +133,7 @@ public class Main {
 
         // Create a new Instance of the Bot, as well as add the Events.
         try {
-            BotWorker.createBot(BotVersion.PUBLIC, "1.7.16");
+            BotWorker.createBot(BotVersion.PUBLIC, "1.7.17");
             instance.musicWorker = new MusicWorker();
             instance.addEvents();
         } catch (Exception ex) {
