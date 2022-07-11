cdf61a611c7c547e8305e1d845e7a262dfe8f519
Presti <dxssucuk@hotmail.com>
2022-07-11 15:35:54 +0200

    * Bump to 1.7.15

--- src/main/java/de/presti/ree6/main/Main.java
+++ src/main/java/de/presti/ree6/main/Main.java
@@ -99,7 +99,7 @@ public class Main {
         // Create a RayGun Client to send Exception to an external Service for Bug fixing.
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             RaygunClient raygunClient = new RaygunClient(instance.config.getConfiguration().getString("raygun.apitoken"));
-            raygunClient.setVersion("1.7.14");
+            raygunClient.setVersion("1.7.15");
         });
 
         // Create a new connection between the Application and the SQL-Server.
@@ -133,7 +133,7 @@ public class Main {
 
         // Create a new Instance of the Bot, as well as add the Events.
         try {
-            BotWorker.createBot(BotVersion.PUBLIC, "1.7.14");
+            BotWorker.createBot(BotVersion.PUBLIC, "1.7.15");
             instance.musicWorker = new MusicWorker();
             instance.addEvents();
         } catch (Exception ex) {
