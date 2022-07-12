4f554edd931ded65fcc971299d685ca17f5b1f2c
DxsSucuk <dxssucuk@hotmail.com>
2022-07-13 01:34:12 +0200

    * Fixed NSFW Commands Slash support. * Improved Rule34 command.

--- src/main/java/de/presti/ree6/commands/impl/nsfw/NSFW.java
+++ src/main/java/de/presti/ree6/commands/impl/nsfw/NSFW.java
@@ -26,67 +26,82 @@ public class NSFW implements ICommand {
 
     @Override
     public void onPerform(CommandEvent commandEvent) {
-        if (commandEvent.getChannel().getType() == ChannelType.TEXT && commandEvent.getChannel().asTextChannel().isNSFW()) {
+        if (commandEvent.isSlashCommand() &&
+                commandEvent.getInteractionHook().getInteraction().getChannel() != null &&
+                commandEvent.getGuild().getTextChannelById(commandEvent.getInteractionHook().getInteraction().getGuild().getId()) != null &&
+                commandEvent.getGuild().getTextChannelById(commandEvent.getInteractionHook().getInteraction().getGuild().getId()).isNSFW()) {
 
-            Message message = commandEvent.isSlashCommand() ?
-                    commandEvent.getInteractionHook().sendMessage("Searching for Image...").complete() :
-                    commandEvent.getChannel().sendMessage("Searching for Image...").complete();
+            sendImage(commandEvent);
+        } else if (commandEvent.getChannel().getType() == ChannelType.TEXT && commandEvent.getChannel().asTextChannel().isNSFW()) {
 
-            JsonElement jsonElement = RequestUtility.request(new RequestUtility.Request("https://www.reddit.com/r/hentai/new.json?sort=hot&limit=50"));
+            sendImage(commandEvent);
+        } else {
+            Main.getInstance().getCommandManager().sendMessage("Only available in NSFW Channels!", 5, commandEvent.getChannel(), commandEvent.getInteractionHook());
+        }
+    }
 
-            if (jsonElement.isJsonObject() &&
-                    jsonElement.getAsJsonObject().has("data") &&
-                    jsonElement.getAsJsonObject().get("data").isJsonObject() &&
-                    jsonElement.getAsJsonObject().getAsJsonObject("data").has("children") &&
-                    jsonElement.getAsJsonObject().getAsJsonObject("data").get("children").isJsonArray()) {
+    /**
+     * Method called to send the Image.
+     *
+     * @param commandEvent the CommandEvent.
+     */
+    public void sendImage(CommandEvent commandEvent) {
+        Message message = commandEvent.isSlashCommand() ?
+                commandEvent.getInteractionHook().sendMessage("Searching for Image...").complete() :
+                commandEvent.getChannel().sendMessage("Searching for Image...").complete();
 
-                JsonArray children = jsonElement.getAsJsonObject().getAsJsonObject("data").getAsJsonArray("children");
+        JsonElement jsonElement = RequestUtility.request(new RequestUtility.Request("https://www.reddit.com/r/hentai/new.json?sort=hot&limit=50"));
 
-                List<String> images = new ArrayList<>();
+        if (jsonElement.isJsonObject() &&
+                jsonElement.getAsJsonObject().has("data") &&
+                jsonElement.getAsJsonObject().get("data").isJsonObject() &&
+                jsonElement.getAsJsonObject().getAsJsonObject("data").has("children") &&
+                jsonElement.getAsJsonObject().getAsJsonObject("data").get("children").isJsonArray()) {
 
-                for (JsonElement child : children) {
-                    if (child.isJsonObject() &&
-                            child.getAsJsonObject().has("data") &&
-                            child.getAsJsonObject().get("data").isJsonObject()) {
+            JsonArray children = jsonElement.getAsJsonObject().getAsJsonObject("data").getAsJsonArray("children");
 
-                        JsonObject data = child.getAsJsonObject().getAsJsonObject("data");
+            List<String> images = new ArrayList<>();
 
-                        if (data.get("url") != null && data.get("url").isJsonPrimitive() &&
-                                data.get("post_hint") != null && data.get("post_hint").isJsonPrimitive()) {
-                            String postHint = data.getAsJsonPrimitive("post_hint").getAsString(),
-                                    fileUrl = data.getAsJsonPrimitive("url").getAsString();
-                            if ((postHint.equalsIgnoreCase("image") ||
-                                    postHint.equalsIgnoreCase("link") ||
-                                    postHint.equalsIgnoreCase("rich:video")) &&
-                                    !fileUrl.toLowerCase(Locale.ROOT).startsWith("https://www.reddit.com/gallery/") &&
-                                    !fileUrl.toLowerCase(Locale.ROOT).startsWith("https://redgifs.com/")) {
-                                images.add(fileUrl);
-                            }
+            for (JsonElement child : children) {
+                if (child.isJsonObject() &&
+                        child.getAsJsonObject().has("data") &&
+                        child.getAsJsonObject().get("data").isJsonObject()) {
+
+                    JsonObject data = child.getAsJsonObject().getAsJsonObject("data");
+
+                    if (data.get("url") != null && data.get("url").isJsonPrimitive() &&
+                            data.get("post_hint") != null && data.get("post_hint").isJsonPrimitive()) {
+                        String postHint = data.getAsJsonPrimitive("post_hint").getAsString(),
+                                fileUrl = data.getAsJsonPrimitive("url").getAsString();
+                        if ((postHint.equalsIgnoreCase("image") ||
+                                postHint.equalsIgnoreCase("link") ||
+                                postHint.equalsIgnoreCase("rich:video")) &&
+                                !fileUrl.toLowerCase(Locale.ROOT).startsWith("https://www.reddit.com/gallery/") &&
+                                !fileUrl.toLowerCase(Locale.ROOT).startsWith("https://redgifs.com/")) {
+                            images.add(fileUrl);
                         }
                     }
                 }
+            }
 
-                if (!images.isEmpty()) {
-                    String randomUrl = images.get(RandomUtils.secureRandom.nextInt(images.size() - 1));
-                    EmbedBuilder em = new EmbedBuilder();
+            if (!images.isEmpty()) {
+                String randomUrl = images.get(RandomUtils.secureRandom.nextInt(images.size() - 1));
+                EmbedBuilder em = new EmbedBuilder();
 
-                    em.setImage(randomUrl);
-                    em.setFooter(commandEvent.getMember().getUser().getAsTag() + " - " + Data.ADVERTISEMENT, commandEvent.getMember().getUser().getAvatarUrl());
+                em.setImage(randomUrl);
+                em.setFooter(commandEvent.getMember().getUser().getAsTag() + " - " + Data.ADVERTISEMENT, commandEvent.getMember().getUser().getAvatarUrl());
 
-                    if (commandEvent.isSlashCommand()) {
-                        message.editMessage("Image found!").queue();
-                        Main.getInstance().getCommandManager().sendMessage(em, commandEvent.getChannel(), null);
-                    } else {
-                        message.editMessageEmbeds(em.build()).queue(message1 -> message1.editMessage("Image found!").queue());
-                    }
+                if (commandEvent.isSlashCommand()) {
+                    message.editMessage("Image found!").queue();
+                    Main.getInstance().getCommandManager().sendMessage(em, commandEvent.getChannel(), null);
                 } else {
-                    message.editMessage("We received an empty Image list from Reddit? Please try again later!").delay(Duration.ofSeconds(5)).flatMap(Message::delete).queue();
+                    message.editMessageEmbeds(em.build()).queue(message1 -> message1.editMessage("Image found!").queue());
                 }
             } else {
-                message.editMessage("We received an Invalid response from Reddit? Please try again later!").delay(Duration.ofSeconds(5)).flatMap(Message::delete).queue();
+                message.editMessage("We received an empty Image list from Reddit? Please try again later!").delay(Duration.ofSeconds(5)).flatMap(Message::delete).queue();
             }
         } else {
-            Main.getInstance().getCommandManager().sendMessage("Only available in NSFW Channels!", 5, commandEvent.getChannel(), commandEvent.getInteractionHook());
+            message.editMessage("We received an Invalid response from Reddit? Please try again later!").delay(Duration.ofSeconds(5)).flatMap(Message::delete).queue();
         }
     }
 
