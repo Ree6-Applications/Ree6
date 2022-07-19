e4c0456c8e3a3d0d66ca56f1d5772c08912d97f9
Presti <dxssucuk@hotmail.com>
2022-07-19 16:20:37 +0200

    * Documentation updated.

--- src/main/java/de/presti/ree6/utils/data/ArrayUtil.java
+++ src/main/java/de/presti/ree6/utils/data/ArrayUtil.java
@@ -17,34 +17,48 @@ public class ArrayUtil {
 
     /**
      * Constructor should not be called, since it is a utility class that doesn't need an instance.
+     *
      * @throws IllegalStateException it is a utility class.
      */
     private ArrayUtil() {
         throw new IllegalStateException("Utility class");
     }
 
-    // HashMap used to store message contents and their IDs, to show the content when the message gets deleted.
+    /**
+     * HashMap used to store message contents and their IDs, to show the content when the message gets deleted.
+     */
     public static final Map<String, Message> messageIDwithMessage = new HashMap<>();
 
-    // HashMap used to store user Ids that are associated with a message, to show the content when the message gets deleted.
+    /**
+     * HashMap used to store user Ids that are associated with a message, to show the content when the message gets deleted.
+     */
     public static final Map<String, User> messageIDwithUser = new HashMap<>();
 
-    // HashMap used to store user Ids and their VC join time, to track VoiceXP.
+    /**
+     * HashMap used to store user Ids and their VC join time, to track VoiceXP.
+     */
     public static final Map<User, Long> voiceJoined = new HashMap<>();
 
-    // HashMap used to store a users Ids, to keep them from spamming commands.
+    /**
+     * HashMap used to store a users Ids, to keep them from spamming commands.
+     */
     public static final ArrayList<String> commandCooldown = new ArrayList<>();
 
-    // HashMap used to store a users Ids, to keep them from earning XP with every message.
+    /**
+     * HashMap used to store a users Ids, to keep them from earning XP with every message.
+     */
     public static final ArrayList<Member> timeout = new ArrayList<>();
 
-    // String Array used to store answer options, for later use by the 8Ball command.
-    public static final String[] answers = new String[] {"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.",
+    /**
+     * String Array used to store answer options, for later use by the 8Ball command.
+     */
+    public static final String[] answers = new String[]{"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.",
             "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
             "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."};
 
     /**
      * Get a String fully of random Number by the given length.
+     *
      * @param length the wanted Length.
      * @return the {@link String} with the wanted Length.
      */
@@ -60,6 +74,7 @@ public class ArrayUtil {
 
     /**
      * Get the User from that send a specific Message that has been deleted.
+     *
      * @param id the ID of the Message.
      * @return the {@link User} that send the Message.
      */
@@ -73,6 +88,7 @@ public class ArrayUtil {
 
     /**
      * Get the Message content, of a deleted Message, by the ID.
+     *
      * @param id the ID of the Message.
      * @return the {@link Message} Entity of the deleted Message.
      */
@@ -86,6 +102,7 @@ public class ArrayUtil {
 
     /**
      * Get the Message content, of a deleted Message, by the ID.
+     *
      * @param id the ID of the Message.
      * @return the {@link Message} Entity of the deleted Message.
      */
