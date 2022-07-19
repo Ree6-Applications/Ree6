dc44dbd59671aeb4b26b5f6e80745cf3d694206b
DxsSucuk <dxssucuk@hotmail.com>
2022-07-19 23:20:55 +0200

    * Fixed Config. * Rollback to SimpleYAML-1.7.2, since 1.8.0 has Issues with default values.

--- src/main/java/de/presti/ree6/utils/data/Config.java
+++ src/main/java/de/presti/ree6/utils/data/Config.java
@@ -2,21 +2,22 @@ package de.presti.ree6.utils.data;
 
 import org.simpleyaml.configuration.file.FileConfiguration;
 import org.simpleyaml.configuration.file.YamlConfiguration;
+import org.simpleyaml.configuration.file.YamlFile;
 
 import java.io.File;
 
 public class Config {
 
-    private FileConfiguration cfg;
+    private YamlFile yamlFile;
 
     public void init() {
 
-        cfg = createConfiguration();
+        yamlFile = createConfiguration();
 
         if (!getFile().exists()) {
-            cfg.options().copyDefaults(true);
-            cfg.options().copyHeader(true);
-            cfg.options().header("""
+            yamlFile.options().copyHeader();
+            yamlFile.options().copyDefaults();
+            yamlFile.options().header("""
                     ################################
                     #                              #
                     # Ree6 Config File             #
@@ -24,42 +25,49 @@ public class Config {
                     #                              #
                     ################################
                     """);
-            cfg.addDefault("mysql.user", "root");
-            cfg.addDefault("mysql.db", "root");
-            cfg.addDefault("mysql.pw", "yourpw");
-            cfg.addDefault("mysql.host", "localhost");
-            cfg.addDefault("mysql.port", 3306);
-            cfg.addDefault("giphy.apitoken", "yourgiphytokenherepog");
-            cfg.addDefault("dagpi.apitoken", "yourdagpixyztokenhere");
-            cfg.addDefault("raygun.apitoken", "yourrayguntokenherepog");
-            cfg.addDefault("spotify.client.id", "yourspotifyclientid");
-            cfg.addDefault("spotify.client.secret", "yourspotifyclientsecret");
-            cfg.addDefault("twitch.client.id", "yourtwitchclientidhere");
-            cfg.addDefault("twitch.client.secret", "yourtwitchclientsecrethere");
-            cfg.addDefault("twitter.consumer.key", "yourTwitterConsumerKey");
-            cfg.addDefault("twitter.consumer.secret", "yourTwitterConsumerSecret");
-            cfg.addDefault("twitter.access.key", "yourTwitterAccessKey");
-            cfg.addDefault("twitter.access.secret", "yourTwitterAccessSecret");
-            cfg.addDefault("youtube.api.key", "youryoutubeapikey");
-            cfg.addDefault("bot.tokens.rel", "ReleaseTokenhere");
-            cfg.addDefault("bot.tokens.dev", "DevTokenhere");
+            yamlFile.addDefault("mysql.user", "root");
+            yamlFile.addDefault("mysql.db", "root");
+            yamlFile.addDefault("mysql.pw", "yourpw");
+            yamlFile.addDefault("mysql.host", "localhost");
+            yamlFile.addDefault("mysql.port", 3306);
+            yamlFile.addDefault("giphy.apitoken", "yourgiphytokenherepog");
+            yamlFile.addDefault("dagpi.apitoken", "yourdagpixyztokenhere");
+            yamlFile.addDefault("raygun.apitoken", "yourrayguntokenherepog");
+            yamlFile.addDefault("spotify.client.id", "yourspotifyclientid");
+            yamlFile.addDefault("spotify.client.secret", "yourspotifyclientsecret");
+            yamlFile.addDefault("twitch.client.id", "yourtwitchclientidhere");
+            yamlFile.addDefault("twitch.client.secret", "yourtwitchclientsecrethere");
+            yamlFile.addDefault("twitter.consumer.key", "yourTwitterConsumerKey");
+            yamlFile.addDefault("twitter.consumer.secret", "yourTwitterConsumerSecret");
+            yamlFile.addDefault("twitter.access.key", "yourTwitterAccessKey");
+            yamlFile.addDefault("twitter.access.secret", "yourTwitterAccessSecret");
+            yamlFile.addDefault("youtube.api.key", "youryoutubeapikey");
+            yamlFile.addDefault("bot.tokens.rel", "ReleaseTokenhere");
+            yamlFile.addDefault("bot.tokens.dev", "DevTokenhere");
 
             try {
-                cfg.save(getFile());
+                yamlFile.save(getFile());
+            } catch (Exception ignored) {
+            }
+        } else {
+            try {
+                yamlFile.load();
             } catch (Exception ignored) {
             }
         }
     }
 
-    public FileConfiguration createConfiguration() {
+    public YamlFile createConfiguration() {
         try {
-            return YamlConfiguration.loadConfiguration(getFile());
+            return new YamlFile(getFile());
         } catch (Exception e) {
-            return new YamlConfiguration();
+            return new YamlFile();
         }
     }
 
-    public FileConfiguration getConfiguration() { return cfg; }
+    public YamlFile getConfiguration() {
+        return yamlFile;
+    }
 
     public File getFile() {
         return new File("config.yml");
