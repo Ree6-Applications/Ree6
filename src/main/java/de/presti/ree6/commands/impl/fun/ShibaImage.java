368aed4825565183f978df39cd0e083623d47e6c
imAETHER <36291026+imAETHER@users.noreply.github.com>
2022-07-19 17:26:52 -0300

    shiba!!!

---
+++ src/main/java/de/presti/ree6/commands/impl/fun/ShibaImage.java
@@ -0,0 +1,53 @@
+package de.presti.ree6.commands.impl.fun;
+
+import com.google.gson.JsonArray;
+import de.presti.ree6.bot.BotWorker;
+import de.presti.ree6.commands.Category;
+import de.presti.ree6.commands.CommandEvent;
+import de.presti.ree6.commands.interfaces.Command;
+import de.presti.ree6.commands.interfaces.ICommand;
+import de.presti.ree6.main.Data;
+import de.presti.ree6.main.Main;
+import de.presti.ree6.utils.external.RequestUtility;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+/**
+ * A command that shows random Shiba inu images, from shibe.online
+ */
+@Command(name = "randomshiba", description = "Shows a random shiba image, from shiba.online .", category = Category.FUN)
+public class ShibaImage implements ICommand {
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void onPerform(CommandEvent commandEvent) {
+        JsonArray js = RequestUtility.request(RequestUtility.Request.builder().url("https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true").build()).getAsJsonArray();
+
+        EmbedBuilder em = new EmbedBuilder();
+
+        em.setTitle("Random Shiba Image!");
+        em.setColor(BotWorker.randomEmbedColor());
+        em.setImage(js.get(0).getAsString());
+        em.setFooter("Requested by " + commandEvent.getMember().getUser().getAsTag() + " - " + Data.ADVERTISEMENT, commandEvent.getMember().getUser().getAvatarUrl());
+
+        Main.getInstance().getCommandManager().sendMessage(em, commandEvent.getChannel(), commandEvent.getInteractionHook());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public CommandData getCommandData() {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String[] getAlias() {
+        return new String[]{"shiba", "shibaimage"};
+    }
+}
