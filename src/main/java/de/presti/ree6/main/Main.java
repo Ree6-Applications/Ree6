870886dd6d9ba018286bff726b18d45353f8200a
Presti <dxssucuk@hotmail.com>
2022-07-20 12:24:49 +0200

    * Bump to 1.7.19. * Fixed YouTube Notifier Issue.

--- src/main/java/de/presti/ree6/main/Main.java
+++ src/main/java/de/presti/ree6/main/Main.java
@@ -99,7 +99,7 @@ public class Main {
         // Create a RayGun Client to send Exception to an external Service for Bug fixing.
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             RaygunClient raygunClient = new RaygunClient(instance.config.getConfiguration().getString("raygun.apitoken"));
-            raygunClient.setVersion("1.7.18");
+            raygunClient.setVersion("1.7.19");
         });
 
         // Create a new connection between the Application and the SQL-Server.
@@ -136,7 +136,7 @@ public class Main {
 
         // Create a new Instance of the Bot, as well as add the Events.
         try {
-            BotWorker.createBot(BotVersion.PUBLIC, "1.7.18");
+            BotWorker.createBot(BotVersion.PUBLIC, "1.7.19");
             instance.musicWorker = new MusicWorker();
             instance.addEvents();
         } catch (Exception ex) {
