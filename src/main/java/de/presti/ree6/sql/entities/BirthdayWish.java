2f5305517e70f561f0775a100d9bae4d070ac3c9
Presti <dxssucuk@hotmail.com>
2022-07-22 15:55:11 +0200

    * Added Birthday-wish Database Entity.

---
+++ src/main/java/de/presti/ree6/sql/entities/BirthdayWish.java
@@ -0,0 +1,94 @@
+package de.presti.ree6.sql.entities;
+
+import de.presti.ree6.sql.base.annotations.Property;
+import de.presti.ree6.sql.base.annotations.Table;
+
+import java.util.Date;
+
+/**
+ * This class is used to represent a Birthday-Wish, in our Database.
+ */
+@Table(name = "BirthdayWish")
+public class BirthdayWish {
+
+    /**
+     * The Guild ID.
+     */
+    @Property(name = "gid")
+    String guildId;
+
+    /**
+     * The Channel ID.
+     */
+    @Property(name = "cid")
+    String channelId;
+
+    /**
+     * The User ID.
+     */
+    @Property(name = "uid")
+    String userId;
+
+    /**
+     * The Birthday.
+     */
+    @Property(name = "birthday")
+    Date birthdate;
+
+    /**
+     * Constructor.
+     */
+    public BirthdayWish() {
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param guildId  the Guild ID.
+     * @param channelId the Channel ID.
+     * @param userId the User ID.
+     * @param birthdate the Birthday.
+     */
+    public BirthdayWish(String guildId, String channelId, String userId, Date birthdate) {
+        this.guildId = guildId;
+        this.channelId = channelId;
+        this.userId = userId;
+        this.birthdate = birthdate;
+    }
+
+    /**
+     * Get the Guild ID.
+     *
+     * @return the Guild ID.
+     */
+    public String getGuildId() {
+        return guildId;
+    }
+
+    /**
+     * Get the Channel ID.
+     *
+     * @return the Channel ID.
+     */
+    public String getChannelId() {
+        return channelId;
+    }
+
+    /**
+     * Get the User ID.
+     *
+     * @return the User ID.
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * Get the Birthday.
+     *
+     * @return the Birthday.
+     */
+    public Date getBirthdate() {
+        return birthdate;
+    }
+}
