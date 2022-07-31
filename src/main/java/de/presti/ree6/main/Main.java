d3b0eebf5d6ba6c60b2a1d54386bcee743d6d841
DxsSucuk <dxssucuk@hotmail.com>
2022-07-31 06:13:53 +0200

    * Version bump. * Switched back to walkyst fork.

--- src/main/java/de/presti/ree6/main/Main.java
+++ src/main/java/de/presti/ree6/main/Main.java
@@ -130,7 +130,7 @@ public class Main {
         // Create a RayGun Client to send Exception to an external Service for Bug fixing.
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             RaygunClient raygunClient = new RaygunClient(instance.config.getConfiguration().getString("raygun.apitoken"));
-            raygunClient.setVersion("1.9.0");
+            raygunClient.setVersion("1.9.1");
         });
 
         // Create a new connection between the Application and the SQL-Server.
