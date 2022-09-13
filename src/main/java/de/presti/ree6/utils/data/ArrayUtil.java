768683f1ee94bb76579b51c9bac1ae85579a8983
DxsSucuk <dxssucuk@hotmail.com>
2022-09-13 22:34:31 +0200

    * Optimized Imports.

--- src/main/java/de/presti/ree6/utils/data/ArrayUtil.java
+++ src/main/java/de/presti/ree6/utils/data/ArrayUtil.java
@@ -1,7 +1,6 @@
 package de.presti.ree6.utils.data;
 
 import de.presti.ree6.utils.others.RandomUtils;
-import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
