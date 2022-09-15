c83bf4173cab170f7e6fd0039f88846b6fd815a8
Presti <dxssucuk@hotmail.com>
2022-09-15 16:33:16 +0200

    Improved GamePlayer

--- src/main/java/de/presti/ree6/game/core/GamePlayer.java
+++ src/main/java/de/presti/ree6/game/core/GamePlayer.java
@@ -1,8 +1,17 @@
 package de.presti.ree6.game.core;
 
+import de.presti.ree6.bot.BotWorker;
+import net.dv8tion.jda.api.entities.User;
+
 public class GamePlayer {
 
     private final long relatedUserId;
+    private User relatedUser;
+
+    public GamePlayer(User relatedUser) {
+        this.relatedUserId = relatedUser.getIdLong();
+        this.relatedUser = relatedUser;
+    }
 
     public GamePlayer(long relatedUserId) {
         this.relatedUserId = relatedUserId;
@@ -10,5 +19,12 @@ public class GamePlayer {
 
     public long getRelatedUserId() {
         return relatedUserId;
+    }
+
+    public User getRelatedUser() {
+        if (relatedUser == null)
+            return relatedUser = BotWorker.getShardManager().getUserById(relatedUserId);
+
+        return relatedUser;
     }
 }
