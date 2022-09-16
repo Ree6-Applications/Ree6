56af1c67804f593883fb56e8a33af8ebc614fad8
Solastis <60881212+Solastis@users.noreply.github.com>
2022-09-16 13:57:25 +0200

    Added Monkey Command to send a monke gif.

--- src/main/java/de/presti/ree6/commands/impl/fun/Monkey.java
+++ src/main/java/de/presti/ree6/commands/impl/fun/Monkey.java
@@ -1,6 +1,5 @@
 package de.presti.ree6.commands.impl.fun;
 
-import com.google.gson.JsonObject;
 import de.presti.ree6.bot.BotWorker;
 import de.presti.ree6.commands.Category;
 import de.presti.ree6.commands.CommandEvent;
@@ -8,12 +7,11 @@ import de.presti.ree6.commands.interfaces.Command;
 import de.presti.ree6.commands.interfaces.ICommand;
 import de.presti.ree6.main.Main;
 import de.presti.ree6.utils.data.Data;
-import de.presti.ree6.utils.external.RequestUtility;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 /**
- * A command to get a random dog images.
+ * Sends a gif of a monkey with a quote.
  */
 @Command(name = "monke", description = "Shows monke.", category = Category.FUN)
 public class Monkey implements ICommand {
@@ -23,15 +21,12 @@ public class Monkey implements ICommand {
 	 */
 	@Override
 	public void onPerform(CommandEvent commandEvent) {
-		JsonObject js = RequestUtility.request(RequestUtility.Request.builder()
-		                                                             .url("https://tenor.com/view/reject-modernity-return-to-monke-monke-gif-19167526")
-		                                                             .build()).getAsJsonObject();
 
-		EmbedBuilder em = new EmbedBuilder();
+		final EmbedBuilder em = new EmbedBuilder();
 
 		em.setTitle("Monke!");
 		em.setColor(BotWorker.randomEmbedColor());
-		em.setImage(js.get("message").getAsString());
+		em.setImage("https://c.tenor.com/Y89PE1f7exQAAAAd/reject-modernity-return-to-monke.gif");
 		em.setFooter("Requested by " + commandEvent.getMember().getUser().getAsTag() + " - " + Data.ADVERTISEMENT,
 		             commandEvent.getMember().getUser().getAvatarUrl()
 		);
