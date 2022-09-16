33cec5855ca6e59a936f7c6e5adbe7d32a042eb2
Presti <dxssucuk@hotmail.com>
2022-09-16 15:55:50 +0200

    Documentation.

--- src/main/java/de/presti/ree6/game/core/GameManager.java
+++ src/main/java/de/presti/ree6/game/core/GameManager.java
@@ -13,12 +13,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Class to manage Games.
+ */
 public class GameManager {
 
+    /**
+     * A HashMap used to cache the Games.
+     * The Key is the Name of the Game.
+     */
     private final static HashMap<String, Class<? extends IGame>> gameCache = new HashMap<>();
 
+    /**
+     * A HashMap used to cache the GameSessions.
+     * The Key is the ID of the Channel.
+     */
     private final static HashMap<String, GameSession> gameSessions = new HashMap<>();
 
+    /**
+     * Method used to create a new GameSession.
+     * @param gameIdentifier The Identifier of the Session.
+     * @param gameName The Name of the Game.
+     * @param channel The Channel where the Game is played.
+     * @param participants The Participants of the Game.
+     * @return The created GameSession.
+     */
     public static GameSession createGameSession(String gameIdentifier, String gameName, MessageChannelUnion channel, ArrayList<User> participants) {
         GameSession gameSession = new GameSession(gameIdentifier, channel, participants);
         gameSession.setGame(getGame(gameName, gameSession));
@@ -26,6 +45,12 @@ public class GameManager {
         return gameSession;
     }
 
+    /**
+     * Method used to get a Game by its name.
+     * @param gameName The Name of the Game.
+     * @param gameSession The GameSession of the Game.
+     * @return The Game.
+     */
     public static IGame getGame(String gameName, GameSession gameSession) {
 
         if (gameCache.containsKey(gameName.toLowerCase().trim())) {
@@ -55,18 +80,36 @@ public class GameManager {
         return null;
     }
 
+    /**
+     * Method used to get a GameSession by its Identifier.
+     * @param gameIdentifier The Identifier of the GameSession.
+     * @return The GameSession.
+     */
     public static GameSession getGameSession(String gameIdentifier) {
         return gameSessions.get(gameIdentifier);
     }
 
+    /**
+     * Method used to get all GameSessions.
+     * @param channel The Channel where the GameSessions are played.
+     * @return A List of GameSessions.
+     */
     public static List<GameSession> getGameSessions(MessageChannelUnion channel) {
         return gameSessions.values().stream().filter(gameSession -> gameSession.getChannel().getId().equals(channel.getId())).toList();
     }
 
+    /**
+     * Method used to get all GameSessions.
+     * @return A List of GameSessions.
+     */
     public static List<GameSession> getGameSessions() {
         return (List<GameSession>) gameSessions.values();
     }
 
+    /**
+     * Method used to remove a GameSession.
+     * @param session The GameSession.
+     */
     public static void removeGameSession(GameSession session) {
         gameSessions.remove(session.getGameIdentifier());
     }
--- src/main/java/de/presti/ree6/game/core/GameSession.java
+++ src/main/java/de/presti/ree6/game/core/GameSession.java
@@ -6,17 +6,48 @@ import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 
 import java.util.ArrayList;
 
+/**
+ * Class used to store Information about Games and its related Information.
+ */
 public class GameSession {
 
+    /**
+     * The Identifier of the GameSession.
+     */
     final String gameIdentifier;
+
+    /**
+     * The Game class.
+     */
     IGame game;
+
+    /**
+     * The Channel where the Game is played.
+     */
     private final MessageChannelUnion channel;
+
+    /**
+     * The Participants of the Game.
+     */
     ArrayList<User> participants;
 
+    /**
+     * Constructor for the GameSession.
+     * @param gameIdentifier The Identifier of the GameSession.
+     * @param messageChannelUnion The Channel where the Game is played.
+     * @param participants The Participants of the Game.
+     */
     public GameSession(String gameIdentifier, MessageChannelUnion messageChannelUnion, ArrayList<User> participants) {
         this(gameIdentifier, null, messageChannelUnion, participants);
     }
 
+    /**
+     * Constructor for the GameSession.
+     * @param gameIdentifier The Identifier of the GameSession.
+     * @param game The Game class.
+     * @param messageChannelUnion The Channel where the Game is played.
+     * @param participants The Participants of the Game.
+     */
     public GameSession(String gameIdentifier, IGame game, MessageChannelUnion messageChannelUnion, ArrayList<User> participants) {
         this.gameIdentifier = gameIdentifier;
         this.game = game;
@@ -24,18 +55,42 @@ public class GameSession {
         this.participants = participants;
     }
 
+    /**
+     * Method used to get the Identifier of the GameSession.
+     * @return The Identifier of the GameSession.
+     */
     public String getGameIdentifier() {
         return gameIdentifier;
     }
+
+    /**
+     * Method used to get the Game class.
+     * @return The Game class.
+     */
     public IGame getGame() {
         return game;
     }
+
+    /**
+     * Method used to set the Game class.
+     * @param game The Game class.
+     */
     public void setGame(IGame game) {
         this.game = game;
     }
+
+    /**
+     * Method used to get the Channel where the Game is played.
+     * @return The Channel where the Game is played.
+     */
     public MessageChannelUnion getChannel() {
         return channel;
     }
+
+    /**
+     * Method used to get the Participants of the Game.
+     * @return The Participants of the Game.
+     */
     public ArrayList<User> getParticipants() {
         return participants;
     }
