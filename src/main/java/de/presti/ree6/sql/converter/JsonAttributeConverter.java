6b7f3e2692e22befd09869ea8c57306b3f121bb1
DxsSucuk <dxssucuk@hotmail.com>
2022-10-24 15:42:50 +0200

    Removed deprecated class.

--- src/main/java/de/presti/ree6/sql/converter/JsonAttributeConverter.java
+++ src/main/java/de/presti/ree6/sql/converter/JsonAttributeConverter.java
@@ -1,10 +1,13 @@
 package de.presti.ree6.sql.converter;
 
-import com.google.gson.JsonElement;
-import de.presti.ree6.utils.data.TypUtil;
+import com.google.gson.*;
 import jakarta.persistence.AttributeConverter;
 
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Blob;
+import java.sql.SQLException;
 
 /**
  * A AttributeConverter to allow us the usage of JsonElements in entities.
@@ -12,11 +15,20 @@ import java.sql.Blob;
 public class JsonAttributeConverter implements AttributeConverter<JsonElement, Blob> {
 
     /**
+     * Instance of GSON.
+     */
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    /**
      * @inheritDoc
      */
     @Override
     public Blob convertToDatabaseColumn(JsonElement attribute) {
-        return TypUtil.convertJSONToBlob(attribute);
+        try {
+            return new SerialBlob(gson.toJson(attribute).getBytes());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -24,6 +36,21 @@ public class JsonAttributeConverter implements AttributeConverter<JsonElement, B
      */
     @Override
     public JsonElement convertToEntityAttribute(Blob dbData) {
-        return TypUtil.convertBlobToJSON(dbData);
+        if (dbData == null)
+            return JsonNull.INSTANCE;
+
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(dbData.getBinaryStream()))) {
+
+            for (String read; (read = reader.readLine()) != null; ) {
+                content.append(read);
+            }
+        } catch (Exception ignore) {
+        }
+
+        if (content.length() == 0)
+            return JsonNull.INSTANCE;
+
+        return JsonParser.parseString(content.toString());
     }
 }
