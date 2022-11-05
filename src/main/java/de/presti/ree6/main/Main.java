e0b6edd2c5e91c0f794afa58741099a696629d7e
DxsSucuk <dxssucuk@hotmail.com>
2022-11-05 14:56:48 +0100

    Version bump.

--- src/main/java/de/presti/ree6/main/Main.java
+++ src/main/java/de/presti/ree6/main/Main.java
@@ -120,7 +120,7 @@ public class Main {
             // We recommend adjusting this value in production.
             options.setTracesSampleRate(1.0);
             // When first trying Sentry it's good to see what the SDK is doing:
-            options.setRelease("2.0.9");
+            options.setRelease("2.0.10");
         });
 
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> Sentry.captureException(e));
@@ -151,7 +151,7 @@ public class Main {
 
         // Create a new Instance of the Bot, as well as add the Events.
         try {
-            BotWorker.createBot(BotVersion.RELEASE, "2.0.9");
+            BotWorker.createBot(BotVersion.RELEASE, "2.0.10");
             instance.musicWorker = new MusicWorker();
             instance.addEvents();
         } catch (Exception ex) {
