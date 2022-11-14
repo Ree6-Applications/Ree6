9386ac35e731aee2827d013762e08197b7508bfd
DxsSucuk <53257574+DxsSucuk@users.noreply.github.com>
2022-11-14 11:02:34 +0100

    Release of the Ree6-Installer

---
+++ Ree6-Installer/src/main/java/de/presti/ree6/Config.java
@@ -0,0 +1,139 @@
+package de.presti.ree6;
+
+import org.simpleyaml.configuration.file.YamlFile;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * Config.
+ */
+public class Config {
+
+    /**
+     * The Configuration.
+     */
+    private YamlFile yamlFile;
+
+    /**
+     * Initialize the Configuration.
+     */
+    public void init() {
+
+        yamlFile = createConfiguration();
+
+        if (!getFile().exists()) {
+            yamlFile.options().copyHeader();
+            yamlFile.options().copyDefaults();
+            yamlFile.options().header("""
+                    ################################
+                    #                              #
+                    # Ree6 Config File             #
+                    # by Presti                    #
+                    #                              #
+                    ################################
+                    """);
+            yamlFile.addDefault("config.version", "2.0.0");
+            yamlFile.addDefault("config.creation", System.currentTimeMillis());
+            yamlFile.addDefault("hikari.sql.user", "root");
+            yamlFile.addDefault("hikari.sql.db", "root");
+            yamlFile.addDefault("hikari.sql.pw", "yourpw");
+            yamlFile.addDefault("hikari.sql.host", "localhost");
+            yamlFile.addDefault("hikari.sql.port", 3306);
+            yamlFile.addDefault("hikari.misc.storage", "sqlite");
+            yamlFile.addDefault("hikari.misc.poolSize", 10);
+            yamlFile.addDefault("dagpi.apitoken", "yourdagpixyztokenhere");
+            yamlFile.addDefault("sentry.dsn", "yourSentryDSNHere");
+            yamlFile.addDefault("spotify.client.id", "yourspotifyclientid");
+            yamlFile.addDefault("spotify.client.secret", "yourspotifyclientsecret");
+            yamlFile.addDefault("twitch.client.id", "yourtwitchclientidhere");
+            yamlFile.addDefault("twitch.client.secret", "yourtwitchclientsecrethere");
+            yamlFile.addDefault("twitter.consumer.key", "yourTwitterConsumerKey");
+            yamlFile.addDefault("twitter.consumer.secret", "yourTwitterConsumerSecret");
+            yamlFile.addDefault("twitter.access.key", "yourTwitterAccessKey");
+            yamlFile.addDefault("twitter.access.secret", "yourTwitterAccessSecret");
+            yamlFile.addDefault("reddit.client.id", "yourredditclientid");
+            yamlFile.addDefault("reddit.client.secret", "yourredditclientsecret");
+            yamlFile.addDefault("instagram.username", "yourInstagramUsername");
+            yamlFile.addDefault("instagram.password", "yourInstagramPassword");
+            yamlFile.addDefault("youtube.api.key", "youryoutubeapikey");
+            yamlFile.addDefault("youtube.api.key2", "youryoutubeapikey2");
+            yamlFile.addDefault("bot.tokens.release", "ReleaseTokenhere");
+            yamlFile.addDefault("bot.tokens.beta", "BetaTokenhere");
+            yamlFile.addDefault("bot.tokens.dev", "DevTokenhere");
+
+            try {
+                yamlFile.save(getFile());
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        } else {
+            try {
+                yamlFile.load();
+                migrateOldConfig();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Migrate from 1.10.0 config to 2.0.0 config.
+     */
+    public void migrateOldConfig() {
+        if (yamlFile.getString("config.version") == null) {
+            Map<String, Object> resources = yamlFile.getValues(true);
+            if (getFile().delete()) {
+                init();
+
+                for (Map.Entry<String, Object> entry : resources.entrySet()) {
+                    String key = entry.getKey();
+
+                    if (key.startsWith("mysql"))
+                        key = key.replace("mysql", "hikari.sql");
+
+                    yamlFile.set(key, entry.getValue());
+                }
+
+                try {
+                    yamlFile.save(getFile());
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Create a new Configuration.
+     * @return The Configuration as {@link YamlFile}.
+     */
+    public YamlFile createConfiguration() {
+        try {
+            return new YamlFile(getFile());
+        } catch (Exception e) {
+            return new YamlFile();
+        }
+    }
+
+    /**
+     * Get the Configuration.
+     * @return The Configuration as {@link YamlFile}.
+     */
+    public YamlFile getConfiguration() {
+        if (yamlFile == null) {
+            init();
+        }
+
+        return yamlFile;
+    }
+
+    /**
+     * Get the Configuration File.
+     * @return The Configuration File as {@link File}.
+     */
+    public File getFile() {
+        return new File("config.yml");
+    }
+
+}
---
+++ Ree6-Installer/src/main/java/de/presti/ree6/Main.java
@@ -0,0 +1,202 @@
+package de.presti.ree6;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Config config = new Config();
+
+    public static void main(String[] args) throws InterruptedException {
+        config.init();
+
+        try {
+            setupStepOne();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            print("Error, will restart!");
+            Thread.sleep(1000);
+            main(args);
+        }
+    }
+
+    public static void print(String print) {
+        System.out.println();
+        System.out.println(print);
+    }
+
+    public static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            else {
+                System.out.print("\033\143");
+            }
+        } catch (IOException | InterruptedException ignore) {}
+    }
+
+    public static void setupStepOne() throws InterruptedException, IOException {
+        clear();
+        print("Welcome to the setup System of Ree6!\nLets start by configuration the Config!\nPlease select one of these Database Types: MariaDB, SQLLite, H2");
+
+        switch (getValueOrDefault("sqllite").toLowerCase()) {
+            case "mariadb" -> {
+                setupMariaDB();
+            }
+
+            case "sqllite" -> {
+                setupSQLLite();
+            }
+
+            default -> {
+                print("Unknown Database Typ!");
+                Thread.sleep(500);
+                setupStepOne();
+            }
+        }
+
+        setupStepTwo();
+    }
+
+    public static void setupStepTwo() throws IOException {
+        clear();
+        print("The Database configuration looks fine!\nLets continue with our API-Keys!\nKeys marked with * are required!");
+
+        print("Enter your Dagpi.xyz-Key (NONE)");
+        config.getConfiguration().set("dagpi.apitoken", getValueOrDefault(""));
+
+        print("Enter your Sentry DSN (NONE)");
+        config.getConfiguration().set("sentry.dsn", getValueOrDefault(""));
+
+        print("Enter your Spotify Client Id (NONE)");
+        config.getConfiguration().set("spotify.client.id", getValueOrDefault(""));
+
+        print("Enter your Spotify Client Secret (NONE)");
+        config.getConfiguration().set("spotify.client.secret", getValueOrDefaultHidden(""));
+
+        print("Enter your Twitch Client Id (NONE)");
+        config.getConfiguration().set("twitch.client.id", getValueOrDefault(""));
+
+        print("Enter your Twitch Client Secret (NONE)");
+        config.getConfiguration().set("twitch.client.secret", getValueOrDefaultHidden(""));
+
+        print("Enter your Twitter Consumer Id (NONE)");
+        config.getConfiguration().set("twitter.consumer.key", getValueOrDefault(""));
+
+        print("Enter your Twitter Consumer Secret (NONE)");
+        config.getConfiguration().set("twitter.consumer.secret", getValueOrDefaultHidden(""));
+
+        print("Enter your Twitter Access Id (NONE)");
+        config.getConfiguration().set("twitter.access.key", getValueOrDefault(""));
+
+        print("Enter your Twitter Access Secret (NONE)");
+        config.getConfiguration().set("twitter.access.secret", getValueOrDefaultHidden(""));
+
+        print("Enter your Reddit Client Id (NONE)");
+        config.getConfiguration().set("reddit.client.id", getValueOrDefault(""));
+
+        print("Enter your Reddit Client Secret (NONE)");
+        config.getConfiguration().set("reddit.client.secret", getValueOrDefaultHidden(""));
+
+        print("Enter your Instagram Account Name (NONE)");
+        config.getConfiguration().set("instagram.username", getValueOrDefault(""));
+
+        print("Enter your Instagram Account password (NONE)");
+        config.getConfiguration().set("instagram.password", getValueOrDefaultHidden(""));
+
+        print("Enter your main YouTube-API-Key (NONE)");
+        config.getConfiguration().set("youtube.api.key", getValueOrDefaultHidden(""));
+
+        print("Enter your secondary YouTube-API-Key (NONE)");
+        config.getConfiguration().set("youtube.api.key2", getValueOrDefaultHidden(""));
+
+        print("Enter your Discord Bot-Token (*)");
+        config.getConfiguration().set("bot.tokens.release", getValueOrDefaultHidden(""));
+
+        config.getConfiguration().save();
+
+        print("Great, we finished setting up everything!\nGive me a second to download the newest JAR!");
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(new URL("https://api.github.com/repos/Ree6-Applications/Ree6/releases"));
+        JsonNode latestReleaseNode = jsonNode.get(0);
+        JsonNode assetsNode = latestReleaseNode.get("assets");
+
+        String tagName = latestReleaseNode.get("tag_name").asText();
+
+        for (Iterator<JsonNode> it = assetsNode.elements(); it.hasNext(); ) {
+            JsonNode asset = it.next();
+            if (asset.get("name").asText().contains("-jar-with-dependencies.jar")) {
+                String downloadUrl = asset.get("browser_download_url").asText();
+
+                try {
+                    IOUtils.copy(new URL(downloadUrl), new File("Ree6.jar"));
+                    print("We downloaded the newest Ree6 version! (" + tagName + ")");
+                } catch (Exception exception) {
+                    print("We could not download the newest Ree6 version!\nManually download it over " + downloadUrl);
+                }
+                break;
+            }
+        }
+    }
+
+    public static void setupMariaDB() throws IOException {
+        clear();
+        config.getConfiguration().set("hikari.misc.storage", "mariadb");
+
+        print("You selected MariaDB!\nLets start by setting up the connection between Ree6 and MariaDB!\nWhat is the MariaDB-User that you want to use? (root)");
+        String name = getValueOrDefault("root");
+        config.getConfiguration().set("hikari.sql.name", name);
+
+        print("What is the host address? (localhost)");
+        String host = getValueOrDefault("localhost");
+        config.getConfiguration().set("hikari.sql.host", host);
+
+        print("What is the host port? (3306)");
+        String port = getValueOrDefault("3306");
+        config.getConfiguration().set("hikari.sql.port", Integer.parseInt(port));
+
+        print("What is the Database name? (root)");
+        String databaseName = getValueOrDefault("root");
+        config.getConfiguration().set("hikari.sql.db", databaseName);
+
+        print("What is the MariaDB-Password? (NONE)");
+        String password = getValueOrDefaultHidden("");
+        config.getConfiguration().set("hikari.sql.pw", password);
+
+        config.getConfiguration().save();
+    }
+
+    public static void setupSQLLite() throws IOException {
+        clear();
+        config.getConfiguration().set("hikari.misc.storage", "sqllite");
+
+        print("You selected SQLLite!\nYou dont need to set up anything for this one!");
+
+        config.getConfiguration().save();
+    }
+
+    public static String getValue() {
+        return System.console().readLine();
+    }
+
+    public static String getValueOrDefault(String defaultValue) {
+        String value = getValue();
+
+        return value.isBlank() ? defaultValue : value;
+    }
+
+    public static String getValueOrDefaultHidden(String defaultValue) {
+        String value = String.valueOf(System.console().readPassword());
+
+        return value.isBlank() ? defaultValue : value;
+    }
+}
