a12323d985876e277c7521f5bef8aad0e2db22eb
DxsSucuk <dxssucuk@hotmail.com>
2023-01-05 23:43:24 +0100

    Cleanup and fixed documentation.

--- src/main/java/de/presti/ree6/news/Announcement.java
+++ src/main/java/de/presti/ree6/news/Announcement.java
@@ -9,16 +9,29 @@ package de.presti.ree6.news;
  */
 public record Announcement(String id, String title, String content) {
 
+    /**
+     * Get the ID of the announcement.
+     * @return The ID of the announcement.
+     */
     @Override
     public String id() {
         return id;
     }
 
+    /**
+     * Get the title of the announcement.
+     *
+     * @return The title of the announcement.
+     */
     @Override
     public String title() {
         return title;
     }
 
+    /**
+     * Get the content of the announcement.
+     * @return The content of the announcement.
+     */
     @Override
     public String content() {
         return content;
