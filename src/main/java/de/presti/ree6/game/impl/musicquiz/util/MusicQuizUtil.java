8aa7f86c74b3148cdda4e014740d13eac8be9145
Presti <dxssucuk@hotmail.com>
2023-01-28 00:18:44 +0100

    Made it ready for release.

--- src/main/java/de/presti/ree6/game/impl/musicquiz/util/MusicQuizUtil.java
+++ src/main/java/de/presti/ree6/game/impl/musicquiz/util/MusicQuizUtil.java
@@ -4,6 +4,8 @@ import de.presti.ree6.game.impl.musicquiz.entities.MusicQuizEntry;
 import de.presti.ree6.utils.apis.SpotifyAPIHandler;
 import de.presti.ree6.utils.apis.YouTubeAPIHandler;
 import de.presti.ree6.utils.others.RandomUtils;
+import de.presti.ree6.utils.others.ThreadUtil;
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
@@ -41,65 +43,69 @@ public class MusicQuizUtil {
     public MusicQuizUtil() {
         instance = this;
 
-        // Spotify "just hits" Playlist.
-        SpotifyAPIHandler.getInstance().getTracks("37i9dQZF1DXcRXFNfZr7Tp").forEach(track -> {
-            ArtistSimplified[] artistSimplified = track.getArtists();
+        ThreadUtil.createThread(x -> {
+            // Spotify "just hits" Playlist.
+            SpotifyAPIHandler.getInstance().getTracks("37i9dQZF1DXcRXFNfZr7Tp").forEach(track -> {
+                ArtistSimplified[] artistSimplified = track.getArtists();
 
-            String url = null;
+                String url = null;
 
-            try {
-                url = YouTubeAPIHandler.getInstance().searchYoutube(track.getName() + " - " + artistSimplified[0].getName());
-            } catch (Exception e) {
-                log.error("Couldn't get Track from ID", e);
-            }
+                try {
+                    url = YouTubeAPIHandler.getInstance().searchYoutube(track.getName() + " - " + artistSimplified[0].getName());
+                } catch (Exception e) {
+                    log.error("Couldn't get Track from ID", e);
+                }
 
-            if (url == null) return;
+                if (url == null) return;
 
-            MusicQuizEntry musicQuizEntry = new MusicQuizEntry(artistSimplified[0].getName(), track.getName(),
-                    Arrays.stream(artistSimplified).skip(1).map(ArtistSimplified::getName).toArray(String[]::new), url);
+                MusicQuizEntry musicQuizEntry = new MusicQuizEntry(artistSimplified[0].getName(), track.getName(),
+                        Arrays.stream(artistSimplified).skip(1).map(ArtistSimplified::getName).toArray(String[]::new), url);
 
-            entries.add(musicQuizEntry);
-        });
+                entries.add(musicQuizEntry);
+            });
 
-        // Spotify "Today's Top Hits" Playlist.
-        SpotifyAPIHandler.getInstance().getTracks("37i9dQZF1DXcBWIGoYBM5M").forEach(track -> {
-            ArtistSimplified[] artistSimplified = track.getArtists();
+            // Spotify "Today's Top Hits" Playlist.
+            SpotifyAPIHandler.getInstance().getTracks("37i9dQZF1DXcBWIGoYBM5M").forEach(track -> {
+                ArtistSimplified[] artistSimplified = track.getArtists();
 
-            String url = null;
+                String url = null;
 
-            try {
-                url = YouTubeAPIHandler.getInstance().searchYoutube(track.getName() + " - " + artistSimplified[0].getName());
-            } catch (Exception e) {
-                log.error("Couldn't get Track from ID", e);
-            }
-            
-            if (url == null) return;
-            
-            MusicQuizEntry musicQuizEntry = new MusicQuizEntry(artistSimplified[0].getName(), track.getName(),
-                    Arrays.stream(artistSimplified).skip(1).map(ArtistSimplified::getName).toArray(String[]::new), url);
+                try {
+                    url = YouTubeAPIHandler.getInstance().searchYoutube(track.getName() + " - " + artistSimplified[0].getName());
+                } catch (Exception e) {
+                    log.error("Couldn't get Track from ID", e);
+                }
 
-            entries.add(musicQuizEntry);
-        });
+                if (url == null) return;
 
-        // Spotify "Rap Caviar" Playlist.
-        SpotifyAPIHandler.getInstance().getTracks("37i9dQZF1DX0XUsuxWHRQd").forEach(track -> {
-            ArtistSimplified[] artistSimplified = track.getArtists();
+                MusicQuizEntry musicQuizEntry = new MusicQuizEntry(artistSimplified[0].getName(), track.getName(),
+                        Arrays.stream(artistSimplified).skip(1).map(ArtistSimplified::getName).toArray(String[]::new), url);
 
-            String url = null;
+                entries.add(musicQuizEntry);
+            });
 
-            try {
-                url = YouTubeAPIHandler.getInstance().searchYoutube(track.getName() + " - " + artistSimplified[0].getName());
-            } catch (Exception e) {
-                log.error("Couldn't get Track from ID", e);
-            }
+            // Spotify "Rap Caviar" Playlist.
+            SpotifyAPIHandler.getInstance().getTracks("37i9dQZF1DX0XUsuxWHRQd").forEach(track -> {
+                ArtistSimplified[] artistSimplified = track.getArtists();
 
-            if (url == null) return;
+                String url = null;
 
-            MusicQuizEntry musicQuizEntry = new MusicQuizEntry(artistSimplified[0].getName(), track.getName(),
-                    Arrays.stream(artistSimplified).skip(1).map(ArtistSimplified::getName).toArray(String[]::new), url);
+                try {
+                    url = YouTubeAPIHandler.getInstance().searchYoutube(track.getName() + " - " + artistSimplified[0].getName());
+                } catch (Exception e) {
+                    log.error("Couldn't get Track from ID", e);
+                }
 
-            entries.add(musicQuizEntry);
-        });
+                if (url == null) return;
+
+                MusicQuizEntry musicQuizEntry = new MusicQuizEntry(artistSimplified[0].getName(), track.getName(),
+                        Arrays.stream(artistSimplified).skip(1).map(ArtistSimplified::getName).toArray(String[]::new), url);
+
+                entries.add(musicQuizEntry);
+            });
+
+            log.info("Loaded {} entries from Spotify.", entries.size());
+        }, Sentry::captureException);
     }
 
 
