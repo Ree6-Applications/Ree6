e2a0e3bc33675736faa655af9671a840ce754d58
Presti <dxssucuk@hotmail.com>
2023-02-16 13:29:40 +0100

    Added warn an infraction command without translation.

---
+++ src/main/java/de/presti/ree6/commands/impl/mod/Warn.java
@@ -0,0 +1,223 @@
+package de.presti.ree6.commands.impl.mod;
+
+import de.presti.ree6.commands.Category;
+import de.presti.ree6.commands.CommandEvent;
+import de.presti.ree6.commands.interfaces.Command;
+import de.presti.ree6.commands.interfaces.ICommand;
+import de.presti.ree6.sql.SQLSession;
+import de.presti.ree6.sql.entities.Punishments;
+import de.presti.ree6.sql.entities.Warning;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Command(name = "warn", description = "command.description.warn", category = Category.MOD)
+public class Warn implements ICommand {
+    @Override
+    public void onPerform(CommandEvent commandEvent) {
+        if (!commandEvent.isSlashCommand()) {
+            commandEvent.reply(commandEvent.getResource("command.perform.onlySlashSupported"));
+            return;
+        }
+
+        if (!commandEvent.getGuild().getSelfMember().hasPermission(Permission.MODERATE_MEMBERS)) {
+            commandEvent.reply(commandEvent.getResource("message.default.needPermission", Permission.MODERATE_MEMBERS.name()), 5);
+            return;
+        }
+
+        if (!commandEvent.getMember().hasPermission(Permission.MODERATE_MEMBERS)) {
+            commandEvent.reply(commandEvent.getResource("message.default.insufficientPermission", Permission.MODERATE_MEMBERS.name()), 5);
+            return;
+        }
+
+        String subCommandGroup = commandEvent.getSlashCommandInteractionEvent().getSubcommandGroup();
+        String subCommand = commandEvent.getSlashCommandInteractionEvent().getSubcommandName();
+        OptionMapping userMapping = commandEvent.getSlashCommandInteractionEvent().getOption("user");
+        OptionMapping warningMapping = commandEvent.getSlashCommandInteractionEvent().getOption("warnings");
+        OptionMapping roleMapping = commandEvent.getSlashCommandInteractionEvent().getOption("role");
+        OptionMapping secondsMapping = commandEvent.getSlashCommandInteractionEvent().getOption("seconds");
+        OptionMapping reasonMapping = commandEvent.getSlashCommandInteractionEvent().getOption("reason");
+        OptionMapping idMapping = commandEvent.getSlashCommandInteractionEvent().getOption("id");
+        Member member = userMapping.getAsMember();
+
+        switch (subCommandGroup) {
+            case "punishments" -> {
+                switch (subCommand) {
+                    case "roleadd" -> {
+                        Punishments punishments = new Punishments();
+                        punishments.setGuildId(commandEvent.getGuild().getIdLong());
+                        punishments.setWarnings(warningMapping.getAsInt());
+                        punishments.setAction(2);
+                        punishments.setRoleId(roleMapping.getAsRole().getIdLong());
+                        SQLSession.getSqlConnector().getSqlWorker().updateEntity(punishments);
+                        commandEvent.reply(commandEvent.getResource("message.warn.punishment.created"));
+                    }
+
+                    case "roleremove" -> {
+                        Punishments punishments = new Punishments();
+                        punishments.setGuildId(commandEvent.getGuild().getIdLong());
+                        punishments.setWarnings(warningMapping.getAsInt());
+                        punishments.setAction(3);
+                        punishments.setRoleId(roleMapping.getAsRole().getIdLong());
+                        SQLSession.getSqlConnector().getSqlWorker().updateEntity(punishments);
+                        commandEvent.reply(commandEvent.getResource("message.warn.punishment.created"));
+                    }
+
+                    case "timeout" -> {
+                        Punishments punishments = new Punishments();
+                        punishments.setGuildId(commandEvent.getGuild().getIdLong());
+                        punishments.setWarnings(warningMapping.getAsInt());
+                        punishments.setAction(1);
+                        punishments.setTimeoutTime(secondsMapping.getAsLong() * 1000);
+                        SQLSession.getSqlConnector().getSqlWorker().updateEntity(punishments);
+                        commandEvent.reply(commandEvent.getResource("message.warn.punishment.created"));
+                    }
+
+                    case "kick" -> {
+                        Punishments punishments = new Punishments();
+                        punishments.setGuildId(commandEvent.getGuild().getIdLong());
+                        punishments.setWarnings(warningMapping.getAsInt());
+                        punishments.setAction(4);
+                        if (reasonMapping != null) punishments.setReason(reasonMapping.getAsString());
+                        SQLSession.getSqlConnector().getSqlWorker().updateEntity(punishments);
+                        commandEvent.reply(commandEvent.getResource("message.warn.punishment.created"));
+                    }
+
+                    case "ban" -> {
+                        Punishments punishments = new Punishments();
+                        punishments.setGuildId(commandEvent.getGuild().getIdLong());
+                        punishments.setWarnings(warningMapping.getAsInt());
+                        punishments.setAction(5);
+                        if (reasonMapping != null) punishments.setReason(reasonMapping.getAsString());
+                        SQLSession.getSqlConnector().getSqlWorker().updateEntity(punishments);
+                        commandEvent.reply(commandEvent.getResource("message.warn.punishment.created"));
+                    }
+
+                    case "list" -> {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (Punishments punishments : SQLSession.getSqlConnector().getSqlWorker().getEntityList(new Punishments(), "SELECT * FROM Punishments WHERE guildId = :gid", Map.of("gid", commandEvent.getGuild().getIdLong()))) {
+                            int action = punishments.getAction();
+                            stringBuilder.append(punishments.getId()).append(" - ").append(punishments.getWarnings()).append(" -> ");
+
+                            switch (action) {
+                                case 1 -> stringBuilder.append("Timeout").append(" for ").append(Duration.ofMillis(punishments.getTimeoutTime()).toSeconds()).append("seconds");
+                                case 2 -> stringBuilder.append("Role-Add").append(" for ").append("<@").append(punishments.getRoleId()).append(">");
+                                case 3 -> stringBuilder.append("Role-Remove").append(" for ").append("<@").append(punishments.getRoleId()).append(">");
+                                case 4 -> stringBuilder.append("Kick").append(" ").append("with").append(" ").append("reason").append(" ").append(punishments.getReason());
+                                case 5 -> stringBuilder.append("Ban").append(" ").append("with").append(" ").append("reason").append(" ").append(punishments.getReason());
+                            }
+
+                            stringBuilder.append("\n");
+                        }
+                        commandEvent.reply(commandEvent.getResource("message.warn.punishment.list", stringBuilder.toString()));
+                    }
+
+                    case "delete" -> {
+                        Punishments punishment = SQLSession.getSqlConnector().getSqlWorker().getEntity(new Punishments(), "SELECT * FROM Punishments WHERE guildId = :gid AND id = :id", Map.of("gid", commandEvent.getGuild().getIdLong(), "id", idMapping.getAsInt()));
+                        if (punishment != null) {
+                            SQLSession.getSqlConnector().getSqlWorker().deleteEntity(punishment);
+                            commandEvent.reply(commandEvent.getResource("message.warn.punishment.deleted"));
+                        } else {
+                            commandEvent.reply(commandEvent.getResource("message.warn.punishment.notFound"));
+                        }
+                    }
+                }
+            }
+
+            default -> {
+                if (commandEvent.getGuild().getSelfMember().canInteract(member) && commandEvent.getMember().canInteract(member)) {
+                    Warning warning = SQLSession.getSqlConnector().getSqlWorker().getEntity(new Warning(), "SELECT * FROM Warning WHERE guildId = :gid AND userId = :uid", Map.of("gid", commandEvent.getGuild().getIdLong(), "uid", member.getIdLong()));
+                    int warnings = warning != null ? warning.getWarnings() + 1 : 1;
+                    if (warning == null) {
+                        warning = new Warning();
+                        warning.setUserId(member.getIdLong());
+                        warning.setGuildId(commandEvent.getGuild().getIdLong());
+                    }
+
+                    warning.setWarnings(warnings);
+
+                    SQLSession.getSqlConnector().getSqlWorker().updateEntity(warning);
+
+                    commandEvent.reply(commandEvent.getResource("message.warn.success", warnings));
+                    Punishments punishment = SQLSession.getSqlConnector().getSqlWorker().getEntity(new Punishments(), "SELECT * FROM Punishments WHERE guildId = :gid AND warnings = :amount", Map.of("gid", commandEvent.getGuild().getIdLong(), "amount", warnings));
+                    if (punishment != null) {
+                        switch (punishment.getAction()) {
+                            case 1 -> {
+                                member.timeoutFor(Duration.ofMillis(punishment.getTimeoutTime())).reason("Reached " + warnings + " warnings!").queue();
+                            }
+                            case 2 -> {
+                                Role role = commandEvent.getGuild().getRoleById(punishment.getRoleId());
+                                if (role == null) {
+                                    SQLSession.getSqlConnector().getSqlWorker().deleteEntity(punishment);
+                                    return;
+                                }
+
+                                commandEvent.getGuild().addRoleToMember(member, role).reason("Reached " + warnings + " warnings!").queue();
+                            }
+                            case 3 -> {
+                                Role role = commandEvent.getGuild().getRoleById(punishment.getRoleId());
+                                if (role == null) {
+                                    SQLSession.getSqlConnector().getSqlWorker().deleteEntity(punishment);
+                                    return;
+                                }
+
+                                commandEvent.getGuild().removeRoleFromMember(member, role).reason("Reached " + warnings + " warnings!").queue();
+                            }
+                            case 4 -> member.kick().reason(punishment.getReason()).queue();
+                            case 5 -> member.ban(0, TimeUnit.DAYS).reason(punishment.getReason()).queue();
+                            default -> log.error("Unhandled action! {}", punishment.getAction());
+                        }
+                    }
+                } else {
+                    if (commandEvent.getGuild().getSelfMember().canInteract(member)) {
+                        commandEvent.reply(commandEvent.getResource("message.warn.hierarchySelfError"), 5);
+                    } else {
+                        commandEvent.reply(commandEvent.getResource("message.warn.hierarchyBotError"), 5);
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public CommandData getCommandData() {
+        return new CommandDataImpl("warn", "command.description.warn")
+                .addOption(OptionType.USER, "user", "The user that should be warned.", true)
+                .addSubcommandGroups(new SubcommandGroupData("punishments", "Set up the punishments for these warnings.")
+                        .addSubcommands(new SubcommandData("timeout", "A Timeout punishment.")
+                                .addOption(OptionType.INTEGER, "warnings", "The needed warning amounts.", true)
+                                .addOption(OptionType.INTEGER, "seconds", "The seconds of how long the timeout should go on.", true))
+                        .addSubcommands(new SubcommandData("roleadd", "A Punishment where you add a role to the user.")
+                                .addOption(OptionType.INTEGER, "warnings", "The needed warning amounts.", true)
+                                .addOption(OptionType.ROLE, "role", "The role the user should receive.", true))
+                        .addSubcommands(new SubcommandData("roleremove", "A Punishment where you remove a role from the user.")
+                                .addOption(OptionType.INTEGER, "warnings", "The needed warning amounts.", true)
+                                .addOption(OptionType.ROLE, "role", "The role the user should get removed.", true))
+                        .addSubcommands(new SubcommandData("kick", "A Kick punishment.")
+                                .addOption(OptionType.INTEGER, "warnings", "The needed warning amounts.", true)
+                                .addOption(OptionType.STRING, "reason", "A custom message", false))
+                        .addSubcommands(new SubcommandData("ban", "A Ban punishment.")
+                                .addOption(OptionType.INTEGER, "warnings", "The needed warning amounts.", true)
+                                .addOption(OptionType.STRING, "reason", "A custom message.", false))
+                        .addSubcommands(new SubcommandData("list", "A list of all punishments."))
+                        .addSubcommands(new SubcommandData("delete", "Delete a specific punishment.")
+                                .addOptions(new OptionData(OptionType.INTEGER, "id", "The Punishment Id.", true).setMinValue(0))));
+    }
+
+    @Override
+    public String[] getAlias() {
+        return new String[0];
+    }
+}
