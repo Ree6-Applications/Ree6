4b38262668ceb91f9e8595a47b9093d82fff6ff4
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2023-04-22 11:34:35 +0300

    Pickup line command, refactored some code over the `Waifu.java`

--- src/main/java/de/presti/ree6/commands/impl/fun/PickUpLine.java
+++ src/main/java/de/presti/ree6/commands/impl/fun/PickUpLine.java
@@ -14,22 +14,23 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import java.awt.*;
 
 /**
- * A command to show you are random Waifu or Husbando.
- */
-@Command(name = "waifu", description = "command.description.waifu", category = Category.FUN)
-public class Waifu implements ICommand {
+ * A command to give you a random pickup line
+ * */
+
+@Command(name = "pickupline", description = "command.description.pickupline", category = Category.FUN)
+public class PickUpLine implements ICommand {
 
     /**
      * @inheritDoc
      */
     @Override
     public void onPerform(CommandEvent commandEvent) {
-        JsonObject jsonObject = RequestUtility.requestJson(RequestUtility.Request.builder().url("https://api.dagpi.xyz/data/waifu")
+        JsonObject jsonObject = RequestUtility.requestJson(RequestUtility.Request.builder().url("https://api.dagpi.xyz/data/pickupline")
                 .bearerAuth(Main.getInstance().getConfig().getConfiguration().getString("dagpi.apitoken")).build()).getAsJsonObject();
 
         EmbedBuilder em = new EmbedBuilder();
 
-        if (!jsonObject.has("series")) {
+        if (!jsonObject.has("category")) {
             em.setTitle("Error!");
             em.setColor(Color.RED);
             em.setDescription(commandEvent.getResource("message.default.retrievalError"));
@@ -38,23 +39,10 @@ public class Waifu implements ICommand {
             return;
         }
 
-        JsonObject jsonObject1 = jsonObject.get("series").getAsJsonObject();
+        commandEvent.reply(":speaking_head:" + ' ' + (jsonObject.has("category")
+                ? jsonObject.get("joke").getAsString()
+                : "I cannot think of a response right now..."));
 
-        em.setImage((jsonObject.has("display_picture") ? jsonObject.get("display_picture").getAsString() : "https://images.ree6.de/notfound.png"));
-        em.addField("**" + commandEvent.getResource("label.character") + "**", "``" + (jsonObject.has("name")
-                ? jsonObject.get("name").getAsString()
-                : commandEvent.getResource("message.default.retrievalError")) + "``", true);
-
-        em.addField("**"  + commandEvent.getResource("label.from") + "**", "``" + (jsonObject1.has("name")
-                ? jsonObject1.get("name").getAsString()
-                : commandEvent.getResource("message.default.retrievalError")) + "``", true);
-
-        if((jsonObject.has("nsfw") && jsonObject.get("nsfw").getAsBoolean())) {
-            em.addField("**NSFW**", "", true);
-        }
-        em.setFooter(commandEvent.getMember().getUser().getAsTag() + " - " + Data.ADVERTISEMENT, commandEvent.getMember().getUser().getAvatarUrl());
-
-        commandEvent.reply(em.build());
     }
 
     /**
