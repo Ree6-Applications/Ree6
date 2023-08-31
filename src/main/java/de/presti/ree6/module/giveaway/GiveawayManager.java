d5b16c9a81e0050b2ab131f45daa6ae022207ba5
DxsSucuk <53257574+DxsSucuk@users.noreply.github.com>
2023-08-31 08:43:02 +0200

    Error fix

--- src/main/java/de/presti/ree6/module/giveaway/GiveawayManager.java
+++ src/main/java/de/presti/ree6/module/giveaway/GiveawayManager.java
@@ -2,23 +2,38 @@ package de.presti.ree6.module.giveaway;
 
 import de.presti.ree6.logger.invite.InviteContainer;
 import de.presti.ree6.module.IManager;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
+/**
+ * Manager for the Giveaways.
+ */
 public class GiveawayManager implements IManager<InviteContainer> {
 
+    /**
+     * Constructor.
+     */
+    public GiveawayManager() {
+        load();
+    }
+
+    /**
+     * The List of Giveaways.
+     */
     private final List<InviteContainer> giveaways = new ArrayList<>();
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public void load() {
 
     }
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public InviteContainer get(String value) {
         for (InviteContainer giveaway : giveaways) {
@@ -30,11 +45,17 @@ public class GiveawayManager implements IManager<InviteContainer> {
         return null;
     }
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public InviteContainer get(long value) {
         return get(String.valueOf(value));
     }
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public List<InviteContainer> getList() {
         return giveaways;
