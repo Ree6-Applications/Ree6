6951339f391d9339166bbaef6bcd5938e512ab7c
Presti <dxssucuk@hotmail.com>
2023-09-05 17:21:39 +0200

    Fix.

--- src/main/java/de/presti/ree6/audio/music/MusicWorker.java
+++ src/main/java/de/presti/ree6/audio/music/MusicWorker.java
@@ -65,7 +65,7 @@ public class MusicWorker {
      */
     public MusicWorker() {
         musicManagers = new HashMap<>();
-        playerManager = new DefaultAudioPlayerManager();
+        playerManager = Data.shouldUseLavaLink() ? Main.getInstance().getLavalink().getAudioPlayerManager() : new DefaultAudioPlayerManager();
 
         // Register AudioSources, if music module is active. If not, then don't register them. This will cause a failed resolve when ever a command is being executed.
         if (Data.isModuleActive("music")) {
