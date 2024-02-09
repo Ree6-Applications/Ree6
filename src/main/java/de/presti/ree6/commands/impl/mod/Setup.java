67b1da4b9ab9c4733b69903a01283d008c3deb50
Presti <dxssucuk@hotmail.com>
2024-02-09 10:52:55 +0100

    Start with function to allow join autorole via setup menu.

--- src/main/java/de/presti/ree6/commands/impl/mod/Setup.java
+++ src/main/java/de/presti/ree6/commands/impl/mod/Setup.java
@@ -11,9 +11,15 @@ import de.presti.ree6.bot.BotConfig;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
 import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
 import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 import net.dv8tion.jda.internal.interactions.component.StringSelectMenuImpl;
 
 import java.awt.*;
@@ -67,35 +73,95 @@ public class Setup implements ICommand {
                 return;
             }
 
-            EmbedBuilder embedBuilder = new EmbedBuilder()
-                    .setTitle(commandEvent.getResource("label.setup"))
-                    .setFooter(commandEvent.getGuild().getName() + " - " + BotConfig.getAdvertisement(), commandEvent.getGuild().getIconUrl())
-                    .setColor(Color.cyan)
-                    .setDescription(commandEvent.getResource("message.setup.setupMenu"));
+            if (commandEvent.getSubcommandGroup().isBlank()) {
+                if (commandEvent.getSubcommand().isBlank()) {
+                    EmbedBuilder embedBuilder = new EmbedBuilder()
+                            .setTitle(commandEvent.getResource("label.setup"))
+                            .setFooter(commandEvent.getGuild().getName() + " - " + BotConfig.getAdvertisement(), commandEvent.getGuild().getIconUrl())
+                            .setColor(Color.cyan)
+                            .setDescription(commandEvent.getResource("message.setup.setupMenu"));
 
-            List<SelectOption> optionList = new ArrayList<>();
-            optionList.add(SelectOption.of(commandEvent.getResource("label.language"), "lang"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.auditLog"), "log"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.welcomeChannel"), "welcome"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.autoRole"), "autorole"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.temporalVoice"), "tempvoice"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.statistics"), "statistics"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.ticketSystem"), "tickets"));
-            optionList.add(SelectOption.of(commandEvent.getResource("label.rewards"), "rewards"));
+                    List<SelectOption> optionList = new ArrayList<>();
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.language"), "lang"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.auditLog"), "log"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.welcomeChannel"), "welcome"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.autoRole"), "autorole"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.temporalVoice"), "tempvoice"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.statistics"), "statistics"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.ticketSystem"), "tickets"));
+                    optionList.add(SelectOption.of(commandEvent.getResource("label.rewards"), "rewards"));
 
-            SelectMenu selectMenu = new StringSelectMenuImpl("setupActionMenu", commandEvent.getResource("message.setup.setupMenuPlaceholder"), 1, 1, false, optionList);
+                    SelectMenu selectMenu = new StringSelectMenuImpl("setupActionMenu", commandEvent.getResource("message.setup.setupMenuPlaceholder"), 1, 1, false, optionList);
 
-            if (commandEvent.isSlashCommand()) {
-                commandEvent.getInteractionHook().sendMessageEmbeds(embedBuilder.build())
-                        .addActionRow(selectMenu).queue();
+                    if (commandEvent.isSlashCommand()) {
+                        commandEvent.getInteractionHook().sendMessageEmbeds(embedBuilder.build())
+                                .addActionRow(selectMenu).queue();
+                    } else {
+                        commandEvent.getChannel().sendMessageEmbeds(embedBuilder.build())
+                                .addActionRow(selectMenu).queue();
+                    }
+                } else if (commandEvent.getSubcommand().equalsIgnoreCase("autorole")) {
+                    // TODO:: update messages.
+                    EmbedBuilder embedBuilder = new EmbedBuilder()
+                            .setTitle(commandEvent.getResource("label.setup"))
+                            .setFooter(commandEvent.getGuild().getName() + " - " + BotConfig.getAdvertisement(), commandEvent.getGuild().getIconUrl())
+                            .setColor(Color.cyan)
+                            .setDescription(commandEvent.getResource("message.setup.setupMenu"));
+
+                    List<SelectOption> optionList = new ArrayList<>();
+
+                    commandEvent.getGuild().getRoles().forEach(role -> optionList.add(SelectOption.of(role.getName(), role.getId())));
+
+                    SQLSession.getSqlConnector().getSqlWorker().getAutoRoles(commandEvent.getGuild().getIdLong()).forEach(autoRole -> {
+                        SelectOption option = optionList.stream().filter(selectOption -> selectOption.getValue().equals(String.valueOf(autoRole.getRoleId()))).findFirst().orElse(null);
+                        if (option != null) {
+                            optionList.remove(option);
+                            optionList.add(option.withDefault(true));
+                        }
+                    });
+
+                    SelectMenu selectMenu = new StringSelectMenuImpl("setupActionMenu", commandEvent.getResource("message.setup.setupMenuPlaceholder"), 0, 10, false, optionList);
+
+                    if (commandEvent.isSlashCommand()) {
+                        commandEvent.getInteractionHook().sendMessageEmbeds(embedBuilder.build())
+                                .addActionRow(selectMenu).queue();
+                    } else {
+                        commandEvent.getChannel().sendMessageEmbeds(embedBuilder.build())
+                                .addActionRow(selectMenu).queue();
+                    }
+                }
             } else {
-                commandEvent.getChannel().sendMessageEmbeds(embedBuilder.build())
-                        .addActionRow(selectMenu).queue();
+                switch (commandEvent.getSubcommandGroup()) {
+                    case "auditlog" -> {
+                        if (commandEvent.getSubcommand().equals("set")) {
+                            // TODO:: log set
+                        } else {
+                            // TODO:: log remove
+                        }
+                    }
+                    case "welcome" -> {
+                        if (commandEvent.getSubcommand().equals("set")) {
+                            // TODO:: welcome set
+                        } else {
+                            // TODO:: welcome remove
+                        }
+                    }
+                    case "autorole" -> {
+                    }
+                    case "tempvoice" -> {
+                        if (commandEvent.getSubcommand().equals("set")) {
+                            // TODO:: temporal set
+                        } else {
+                            // TODO:: temporal remove
+                        }
+                    }
+                }
             }
         } else {
             commandEvent.reply(commandEvent.getResource("message.default.insufficientPermission", Permission.ADMINISTRATOR.name() + "/" + Permission.MANAGE_SERVER.name()));
         }
         Main.getInstance().getCommandManager().deleteMessage(commandEvent.getMessage(), commandEvent.getInteractionHook());
+
     }
 
     /**
@@ -103,7 +169,33 @@ public class Setup implements ICommand {
      */
     @Override
     public CommandData getCommandData() {
-        return null;
+        return new CommandDataImpl("setup", "command.description.setup")
+                .addSubcommands(new SubcommandData("menu", "Open the Setup Menu."))
+                .addSubcommands(new SubcommandData("autorole", "Configure the Autoroles."))
+                .addSubcommandGroups(
+                        new SubcommandGroupData("auditlog", "Auditlog Setup")
+                                .addSubcommands(new SubcommandData("set", "Set the Auditlog channel.")
+                                        .addOptions(new OptionData(OptionType.CHANNEL, "channel", "The Welcome Channel.", true).setChannelTypes(ChannelType.TEXT)))
+                                .addSubcommands(new SubcommandData("remove", "Remove the Auditlog channel.")),
+                        new SubcommandGroupData("welcome", "Welcome Setup")
+                                .addSubcommands(new SubcommandData("set", "Set the Welcome channel.")
+                                        .addOptions(new OptionData(OptionType.CHANNEL, "channel", "The Auditlog Channel.", true).setChannelTypes(ChannelType.TEXT)))
+                                .addSubcommands(new SubcommandData("remove", "Remove the Welcome channel.")),
+                        new SubcommandGroupData("tempvoice", "Temporal Voice Setup")
+                                .addSubcommands(new SubcommandData("set", "Set a Temporal Voice Channel.")
+                                        .addOptions(new OptionData(OptionType.CHANNEL, "channel", "The Temporal Voice Channel.", true).setChannelTypes(ChannelType.VOICE)))
+                                .addSubcommands(new SubcommandData("remove", "Remove a Temporal Voice Channel.")),
+                        new SubcommandGroupData("statistics", "Statistics Setup")
+                                .addSubcommands(new SubcommandData("create", "Create the Statistics channel."))
+                                .addSubcommands(new SubcommandData("remove", "Remove the Statistics channel.")),
+                        new SubcommandGroupData("tickets", "Ticket System Setup")
+                                .addSubcommands(new SubcommandData("set", "Set the Ticket System channel.")
+                                        .addOptions(new OptionData(OptionType.CHANNEL, "channel", "The Ticket Channel.", true).setChannelTypes(ChannelType.TEXT)))
+                                .addSubcommands(new SubcommandData("remove", "Remove the Ticket System channel.")),
+                        new SubcommandGroupData("rewards", "Rewards Setup")
+                                .addSubcommands(new SubcommandData("set", "Set the Rewards value."))
+                                .addSubcommands(new SubcommandData("reset", "Reset the Rewards value."))
+                );
     }
 
     /**
